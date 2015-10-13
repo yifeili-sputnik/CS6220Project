7a245de89fca764a6061084887734fbd1b8b7e02
Yifei Li <yifeili.sputnik@gmail.com>
2015-10-13 19:55:19 -0400

    Modified logic error when iterate between players.

--- src/JsonParserTest.java
+++ src/JsonParserTest.java
@@ -13,21 +13,24 @@ public class JsonParserTest {
 	public static void main(String[] args) throws IOException, IOException {
 		Gson gson = new Gson();
 		try {
-			BufferedReader br = new BufferedReader(new FileReader("data/1852823265.json"));
+			BufferedReader br = new BufferedReader(new FileReader("data/1852823253.json"));
 			MatchObject mo = gson.fromJson(br, MatchObject.class);
 			Result res = mo.getResult();
 			int matchDuration = res.getDuration() / 60;
 			int radientTotalKill = 0;
 			int radientTotalLH = 0;
-			for (int i = 0; i < 5; i++) {
-				for (Player p : res.getPlayers()) {
-					radientTotalKill += p.getKills();
-					radientTotalLH += p.getLast_hits();
-				}
+			int i = 0;
+			for (Player p : res.getPlayers()) {
+				if (i >= 5)
+					break;
+				i++;
+				radientTotalKill += p.getKills();
+				radientTotalLH += p.getLast_hits();
 			}
-			System.out.println("Radient win: "+res.isRadiant_win() );
-			System.out.println("Radient average Kills/min: "+ (double)radientTotalKill/matchDuration);
-			System.out.println("Radient average Last_hits/min: "+ (double)radientTotalLH/matchDuration);
+
+			System.out.println("Radient win: " + res.isRadiant_win());
+			System.out.println("Radient average Kills/min: " + (double) radientTotalKill / matchDuration);
+			System.out.println("Radient average Last_hits/min: " + (double) radientTotalLH / matchDuration);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
