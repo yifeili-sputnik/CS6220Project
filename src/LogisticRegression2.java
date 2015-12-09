18fc6ed74d3b641c94c2dded09b2d3ce28c81d91
Yifei Li <yifeili.sputnik@gmail.com>
2015-12-09 12:18:45 -0500

    Optimizing minsupp value

--- src/LogisticRegression2.java
+++ src/LogisticRegression2.java
@@ -40,7 +40,7 @@ public class LogisticRegression2 {
 	public void train(List<MatchObject> mObjects) {
 		// construct train matrix
 		int teamNumber = mObjects.size() * 2;
-		FrequentPattern fp = new FrequentPattern(mObjects, (int) (mObjects.size() * 5 / 112 / 2 / 1.5));
+		FrequentPattern fp = new FrequentPattern(mObjects, (int) (mObjects.size() * 5 / 112 / 2 / 2));
 		fp.Apriori();
 
 		int[][] trainMatrix = new int[teamNumber][Constants.Cols2];
